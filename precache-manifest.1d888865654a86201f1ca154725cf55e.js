f619d0f6f54291def19e0b03931f5cc71b3efd4a
Simon BERNARD <simon.bernard@polyconseil.fr>
2020-03-23 11:11:22 +0100

    build v1

---
+++ precache-manifest.1d888865654a86201f1ca154725cf55e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "221869e9d699d13bb52647d743127728",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "e9cd2512ef235026429d",
+    "url": "/covid19/static/css/main.3f335114.chunk.css"
+  },
+  {
+    "revision": "2cda7b6cd9229ab6a19b",
+    "url": "/covid19/static/js/2.03868fcb.chunk.js"
+  },
+  {
+    "revision": "e7cad5e8c80a4a756b57f883a8b131b2",
+    "url": "/covid19/static/js/2.03868fcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f02f9e57175f9b7d655f",
+    "url": "/covid19/static/js/3.4d338945.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/covid19/static/js/3.4d338945.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e6fad7de7abcdad91ce",
+    "url": "/covid19/static/js/4.4da2ef14.chunk.js"
+  },
+  {
+    "revision": "e9cd2512ef235026429d",
+    "url": "/covid19/static/js/main.b96044c6.chunk.js"
+  },
+  {
+    "revision": "8324703d5937cad19a1d",
+    "url": "/covid19/static/js/runtime-main.4d93bc56.js"
+  }
+]);
